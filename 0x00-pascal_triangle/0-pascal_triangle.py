a67f1bf05bbed6503f8e9db6f9ef55349626b02d
Ezekwe chibuikem <nwachinaemelum@yahoo.com>
2024-10-06 22:40:38 +0100

    task one

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Pascal's triangle generator.
+"""
+
+
+def pascal_triangle(n):
+
+    """
+    Generate Pascal's triangle of n rows.
+
+    """
+    if n <= 0:
+        return []
+
+    # Initialize the triangle with the first row
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
